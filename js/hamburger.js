8661aaeddce33fc8ee3d219f07e2e5571bfdcb67
iamyujinwon <iamyujinwon@gmail.com>
2020-12-18 12:16:27 -0500

    edit nav-bar

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,9 +5,10 @@ menuIcon.addEventListener('click', ()=> {
     navbar.classList.toggle('change');
 });
 
-// const navLink = document.querySelector('.nav-link');
 
-// navLink.addEventListener('click', ()=> {
-//     console.log('hell0');
-//     navbar.classList.toggle('change');
-// });
+/*home이나 다른 선택 사항 눌러도 제거 되겠금 설치!*/
+const navList = document.querySelector('.nav-list');
+
+navList.addEventListener('click', ()=> {
+    navbar.classList.toggle('change');
+});
